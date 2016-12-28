72bb3ec7c11aa768b486d6f3c62d72e6a6315f40
Daidado <admin@daidado.com>
2016-12-27 18:57:38 -0600

    add first pass at the elections data

--- bin/elections.go
+++ bin/elections.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"elections"
+	"log"
+	"os"
 )
 
 func main() {
-	elections.PrintDerp()
+	file, err := os.Open("data/elections/US_County_Level_Presidential_Results_08-16.csv")
+	if err != nil {
+		log.Fatalf("ono! %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var counties []elections.County
+	for scanner.Scan() {
+		counties = append(counties, elections.ParseRow(scanner.Text()))
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("poo %v", err)
+	}
+
+	rToD, dToR := elections.GetFlippedCounties0816(counties)
+	log.Printf("08-16: R->D: %d, D->R: %d", len(rToD), len(dToR))
+
+	rToD, dToR = elections.GetFlippedCounties1216(counties)
+	log.Printf("12-16: R->D: %d, D->R: %d", len(rToD), len(dToR))
+
+	for _, flip := range dToR {
+		log.Printf("%d", flip.ID)
+	}
 }
