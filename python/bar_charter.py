cb5b0880c268e5b9eb81c64b3531c855de55c678
DaiDaDo <admin@daidado.com>
2017-01-08 16:57:48 -0600

    first draft income bar chart giffer

--- python/bar_charter.py
+++ python/bar_charter.py
@@ -1,35 +1,45 @@
 #!/usr/bin/env python
 # a bar plot with errorbars
 import numpy as np
-import matplotlib.pyplot as plt
+import sys
+from matplotlib import pyplot
+from PIL import Image
+# https://pypi.python.org/pypi/images2gif
+from images2gif import writeGif
 
-N = 5
-menMeans = (20, 35, 30, 35, 27)
-menStd = (2, 3, 4, 1, 2)
+def plot(year, rawnums, outpath):
+    nums1967 = [ 3000, 5850, 8303, 11840, 19000]
+    percentages = [ 0, 0, 0, 0, 0]
+    for i in range(0, 5):
+        percentages[i] = rawnums[i] * nums1967[4] / nums1967[i] / rawnums[4]
 
-ind = np.arange(N)  # the x locations for the groups
-width = 0.55       # the width of the bars
+    ind = np.arange(5)  # the x locations for the groups
+    width = 0.55       # the width of the bars
+    fig, ax = pyplot.subplots()
+    rects = ax.bar(ind, percentages, width, color='r')
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind, menMeans, width, color='r')
+    # add some text for labels, title and axes ticks
+    ax.set_ylim([0, 1.05])
+    ax.set_ylabel('Income Cutoff (Adjusted to 1967 inequality ratios)')
+    ax.set_title('Change in Wealth Inequality from 1967 to {0}'.format(year),
+                 fontweight='bold')
+    ax.set_xticks(ind + width)
+    ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
 
-# add some text for labels, title and axes ticks
-ax.set_ylabel('Income Cutoff (Adjusted to 1967 inequality ratios)')
-ax.set_title('Change in Wealth Inequality from 1967 to 1968',
-             fontweight='bold')
-ax.set_xticks(ind + width)
-ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+    pyplot.tick_params(axis='x', bottom='off', top='off', which='both')
+    pyplot.savefig(outpath)
+    pyplot.close()
 
-#ax.legend(rects1[0], 'Men')
+if __name__ == '__main__':
+    f = open(sys.argv[1])
+    f.readline()
+    rows = f.readlines()
+    images = []
+    for row in rows:
+        print row
+        data = row.split(',')
+        filepath = '{0}/{1}.png'.format(sys.argv[2], data[0])
+        plot(data[0], [float(data[1]), float(data[2]), float(data[3]), float(data[4]), float(data[5])], filepath)
+        images.append(Image.open(filepath))
 
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width(), 1.05*height,
-                '%d' % int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-
-plt.show()
+    writeGif('{0}/income.gif'.format(sys.argv[2]), images, .07)
