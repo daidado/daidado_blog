c0e9273ef9d138b79897ee452dfbf3ccc1f45e5d
DaiDaDo <admin@daidado.com>
2017-03-02 20:19:01 -0600

    idk

--- python/pie_charter.py
+++ python/pie_charter.py
@@ -11,18 +11,22 @@ from matplotlib import pyplot
 from StringIO import StringIO
 
 def makeoneyear(labels, values, title, outpath):
+    newvalues = [int(values[0]) + int(values[1]), int(values[2]), int(values[3]) + int(values[4])]
+    newlabels = ["Democrats (w/ leans)", "Independents", "Republicans (w/ leans)"]
+    print values
     pyplot.figure(1, figsize=(8,8))
     pyplot.axes([.15, .25, .7, .7])
 
-    legend = ['{0}: {1:05.2f}%'.format(i,float(j)) for i,j in zip(labels, values)]
+    legend = ['{0}: {1:02d}%'.format(i,int(j)) for i,j in zip(newlabels, newvalues)]
 
     # Plot
-    pyplot.pie(values, shadow=False)
+    colors = ['blue', 'lightgrey', 'red']
+    pyplot.pie(newvalues, shadow=False, colors=colors)
     pyplot.legend(legend,
                   loc='center',
                   bbox_to_anchor=[0.5, -.125],
                   fontsize=10)
-    pyplot.title(title, fontsize=20, fontweight='bold')
+    pyplot.title(title, fontsize=16, fontweight='bold')
     pyplot.savefig(outpath)
 
 if __name__ == '__main__':    
@@ -31,6 +35,8 @@ if __name__ == '__main__':
     labels = [s.replace("\n", "") for s in data.readline().split(",")[1:]]
     
     images = []
+    filepath = ""
+    derr = True
     for l in data:
         yearCsv = l.split(',')
         newTitle = '{0}: {1}'.format(title, yearCsv[0])
@@ -40,7 +46,18 @@ if __name__ == '__main__':
                     newTitle,
                     filepath)
         images.append(Image.open(filepath))
-
+        if derr:
+            images.append(Image.open(filepath))
+            images.append(Image.open(filepath))
+            images.append(Image.open(filepath))
+            images.append(Image.open(filepath))
+            derr = False
+        
+    images.append(Image.open(filepath))
+    images.append(Image.open(filepath))
+    images.append(Image.open(filepath))
+    images.append(Image.open(filepath))
+    images.append(Image.open(filepath))
     writeGif('{0}/{1}.gif'.format(sys.argv[1], title),
              images,
              float(sys.argv[2]))
